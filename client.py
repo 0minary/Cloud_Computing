0d5fde2a281e0b2eea963fbb61a094db47ecb565
Alexis Moisy <alexis.moisy@etudiant.univ-lr.fr>
2025-01-27 15:43:51 +0100

    e

--- client.py
+++ client.py
@@ -28,8 +28,6 @@ def get_test():
     except requests.exceptions.RequestException as e:
         return jsonify({"error": "Impossible de récupérer les données","details": str(e)}), 500
 
-if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=5000)
     
 def get_test():
     """Consomme l'endpoint /test de l'API FastAPI."""
@@ -43,4 +41,4 @@ def get_test():
         "details": str(e)}), 500
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    app.run(debug=True, host="0.0.0.0", port=5001)
